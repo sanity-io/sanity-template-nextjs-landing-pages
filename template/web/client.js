fd41ea4c5e2c306a6fb1dad477594184cfb6967a
Marius Lundgård <studio@mariuslundgard.com>
2019-03-15 12:08:25 +0100

    Add template tools and variables

--- template/web/client.js
+++ template/web/client.js
@@ -1,9 +1,9 @@
-const sanityClient = require('@sanity/client')
+const sanityClient = require("@sanity/client");
 const client = sanityClient({
-  projectId: 'pcgsfgel',
-  dataset: 'production',
-  token: '', // or leave blank to be anonymous user
+  projectId: "<#< sanity.projectId >#>",
+  dataset: "<#< sanity.dataset >#>",
+  token: "", // or leave blank to be anonymous user
   useCdn: false // `false` if you want to ensure fresh data
-})
+});
 
-module.exports = client
+module.exports = client;
