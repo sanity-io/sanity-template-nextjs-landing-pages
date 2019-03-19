87e76405cbcdb48f7a1bb8924084b816891b6c7f
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-03-19 16:51:55 +0100

    Minify css

--- template/web/postcss.config.js
+++ template/web/postcss.config.js
@@ -1,8 +1,9 @@
-module.exports = {
+module.exports = ({ file, options, env }) => ({
   plugins: {
     'postcss-import': {},
+    'cssnano': env === 'production' ? {} : false,
     'postcss-preset-env': {
       stage: 0
     }
   }
-}
+})
