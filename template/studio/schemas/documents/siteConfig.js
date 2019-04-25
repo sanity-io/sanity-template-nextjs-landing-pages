e406a93a223821e29c0131a07447b60b08025fca
Knut Melvær <knut.melvaer@gmail.com>
2019-04-25 11:27:18 +0200

    description + validation

--- template/studio/schemas/documents/siteConfig.js
+++ template/studio/schemas/documents/siteConfig.js
@@ -20,12 +20,13 @@ export default {
     {
       name: 'frontpage',
       type: 'reference',
+      description: 'Choose page to be the frontpage',
       to: { type: 'page' }
     },
     {
       title: 'Site language',
       description:
-        'Shuld be a valid bcp47 language code like en, en-US, no or nb-NO',
+        'Should be a valid bcp47 language code like en, en-US, no or nb-NO',
       name: 'lang',
       type: 'string',
       validation: Rule =>
@@ -38,11 +39,28 @@ export default {
       description:
         'Best choice is to use an SVG where the color are set with currentColor',
       name: 'logo',
-      type: 'image'
+      type: 'image',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+          description:
+            'Important for SEO and accessiblity.',
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ]
     },
     {
       title: 'Main navigation',
       name: 'mainNavigation',
+      description: 'Select pages for the top menu',
+      validation: Rule => [
+        Rule.max(5).warning('Are you sure you want more than 5 items?'),
+        Rule.unique().error('You have duplicate menu items'),
+      ],
       type: 'array',
       of: [
         {
@@ -55,6 +73,10 @@ export default {
       title: 'Footer navigation items',
       name: 'footerNavigation',
       type: 'array',
+      validation: Rule => [
+        Rule.max(10).warning('Are you sure you want more than 10 items?'),
+        Rule.unique().error('You have duplicate menu items'),
+      ],
       fieldset: 'footer',
       of: [
         {
