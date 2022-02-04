6312027fd83f1417eaae0cd8ed9ac9dfd4f94740
Henrique Doro <opensource@hdoro.dev>
2022-02-04 11:34:38 -0300

    fix: hide broken sanity-tutorials dashboard widget

--- template/studio/dashboardConfig.js
+++ template/studio/dashboardConfig.js
@@ -1,12 +1,12 @@
 export default {
   widgets: [
-    {
-      name: 'sanity-tutorials',
-      options: {
-        templateRepoId: 'sanity-io/sanity-template-nextjs-landing-pages'
-      }
-    },
-    {name: 'structure-menu'},
+    // {
+    //   name: 'sanity-tutorials',
+    //   options: {
+    //     templateRepoId: 'sanity-io/sanity-template-nextjs-landing-pages'
+    //   }
+    // },
+    { name: 'structure-menu' },
     {
       name: 'project-info',
       options: {
@@ -39,15 +39,15 @@ export default {
             value: 'https://github.com/<#<repository.owner>#>/<#<repository.name>#>',
             category: 'Code'
           },
-          {title: 'Frontend', value: '<#<deployments.web.url>#>', category: 'apps'}
+          { title: 'Frontend', value: '<#<deployments.web.url>#>', category: 'apps' }
         ]
       }
     },
-    {name: 'project-users', layout: {height: 'auto'}},
     {
       name: 'document-list',
-      options: {title: 'Recently edited', order: '_updatedAt desc', limit: 10, types: ['page']},
-      layout: {width: 'medium'}
-    }
+      options: { title: 'Recently edited', order: '_updatedAt desc', limit: 10, types: ['page'] },
+      layout: { width: 'medium' }
+    },
+    { name: 'project-users', layout: { height: 'auto' } }
   ]
 }
