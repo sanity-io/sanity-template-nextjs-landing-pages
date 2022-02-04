40b72fd15baab1c632d71cf89b6962ecdb52a783
Henrique Doro <opensource@hdoro.dev>
2022-02-04 09:15:29 -0300

    chore: align & simplify ESLint and Prettier

--- template/web/.eslintrc.js
+++ template/web/.eslintrc.js
@@ -1,13 +1,7 @@
 module.exports = {
   extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
   rules: {
-    'object-curly-spacing': ['error', 'never']
+    'object-curly-spacing': ['error', 'never'],
+    'comma-dangle': ['never'],
   },
-  settings: {
-    react: {
-      pragma: 'React',
-      version: '16.8.4'
-    }
-  },
-  parser: 'babel-eslint'
 }
